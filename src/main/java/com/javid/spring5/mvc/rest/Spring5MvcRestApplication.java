a8a3eae324f93646dd0eef642d66d57dd7f1411f
javaj-r <javid.rafiee@gmail.com>
2021-03-20 13:53:58 +0330

    Initializing project. Closes #1.

---
+++ src/main/java/com/javid/spring5/mvc/rest/Spring5MvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.javid.spring5.mvc.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5MvcRestApplication.class, args);
+	}
+
+}
