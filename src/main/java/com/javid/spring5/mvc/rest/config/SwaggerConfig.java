f0c6a2565bc28a1702096f0ef54dac2b7249a495
javaj-r <javid.rafiee@gmail.com>
2021-03-31 00:17:30 +0430

    Configuring Swagger. Closes #34.

---
+++ src/main/java/com/javid/spring5/mvc/rest/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.javid.spring5.mvc.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Javid on 3/30/2021.
+ */
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+}
